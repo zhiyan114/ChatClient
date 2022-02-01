9362f43fa3e60c144f33cefe53303f638f20969a
zhiyan114 <20156291+zhiyan114@users.noreply.github.com>
2022-01-31 21:12:47 -0500

    Initial Commit

---
+++ Server/Communicate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Name: Communicate.cs
+ * Description: Standardized communication protocol between the server and the client
+ * Author: zhiyan114
+ */
+
+/*
+ * Library Implementation:
+ * - Name: protobuf-net (2.4.6)
+ *  - Repository: https://github.com/protobuf-net/protobuf-net
+ */
+namespace Server
+{
+    class Communicate
+    {
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            while(true)
+            {
+                string command = Console.ReadLine();
+                Console.WriteLine("EXECUTED: " + command);
+            }
+        }
+    }
+}
